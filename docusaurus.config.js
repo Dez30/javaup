3482524c2c7a8758386a88e1ab89d32748a0e7dc
阿星不是程序员 <1031900093@qq.com>
2025-11-26 14:51:27 +0800

    添加hashmap内容

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -112,7 +112,7 @@ const config = {
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/shining-stars-l/javaup',
             label: 'GitHub',
             position: 'right',
           },
