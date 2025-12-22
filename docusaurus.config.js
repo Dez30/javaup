6f9c6e642b91c824166784187b480bd4119a1dff
阿星不是程序员 <1031900093@qq.com>
2025-12-22 10:23:00 +0800

    去掉全局的keywords和description

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -162,24 +162,6 @@ var _hmt = _hmt || [];
     },
   ],
 
-  // SEO 全局元数据
-  headTags: [
-    {
-      tagName: 'meta',
-      attributes: {
-        name: 'keywords',
-        content: 'Java面试,Java八股文,Spring,SpringBoot,MyBatis,Redis,MySQL,微服务,分布式,JVM,并发编程,设计模式',
-      },
-    },
-    {
-      tagName: 'meta',
-      attributes: {
-        name: 'description',
-        content: 'JavaUp技术实战 - 提供Java面试八股文、Spring全家桶、微服务架构、分布式系统、数据库优化等全栈技术知识，包含黑马点评升级版、大麦等实战项目讲解。',
-      },
-    },
-  ],
-
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
